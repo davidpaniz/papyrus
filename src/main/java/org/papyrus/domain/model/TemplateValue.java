ace1530a30e0487d5bf7f3f90d14831efbec2d4a
David Paniz <davidpaniz@gmail.com>
2009-07-23 09:23:06 -0300

    creating entity TemplateValue and change frontend to use it

---
+++ src/main/java/org/papyrus/domain/model/TemplateValue.java
@@ -0,0 +1,55 @@
+package org.papyrus.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "TemplateValue_Seq")
+public class TemplateValue {
+	@Id
+	@GeneratedValue(generator = "TemplateValue_Seq", strategy = GenerationType.AUTO)
+	private long id;
+
+	private String field;
+	private String value;
+
+	@ManyToOne
+	private Action action;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Action getAction() {
+		return action;
+	}
+
+	public void setAction(Action action) {
+		this.action = action;
+	}
+
+}
