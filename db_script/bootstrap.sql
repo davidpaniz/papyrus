da97391616afe1dba1ce30ad35755fb9e417c5a5
David Paniz <davidpaniz@gmail.com>
2009-09-09 18:59:16 -0300

    Fixing Notifications Layout and creating OpenIncident Form to final clients

--- db_script/bootstrap.sql
+++ db_script/bootstrap.sql
@@ -1,5 +1,18 @@
+--User
 insert into `User` (email, password, active, role) values( 'a@a.com', '123', true, 'ADMIN');
 insert into `User` (email, password, active, role) values( 's@a.com', '123', true, 'STAFF');
 insert into `User` (email, password, active, role) values( 'm@a.com', '123', true, 'MANAGER');
 insert into `User` (email, password, active, role) values( 'c@a.com', '123', true, 'CLIENT');
 
+--Urgency
+INSERT INTO Urgency(description) values('LOW')
+INSERT INTO Urgency(description) values('MEDIUM')
+INSERT INTO Urgency(description) values('HIGH')
+
+--Impact
+INSERT INTO Impact(description) values('LOW')
+INSERT INTO Impact(description) values('MEDIUM')
+INSERT INTO Impact(description) values('HIGH')
+
+--Priority
+--INSERT INTO Impact(description) values('MEDIUM')
