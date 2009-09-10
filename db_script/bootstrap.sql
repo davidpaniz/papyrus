6d92f4b8687716b6d9781eb34e26b6db05c365ec
David Paniz <davidpaniz@gmail.com>
2009-09-10 18:55:05 -0300

    add Priority to bootstrap.sql

--- db_script/bootstrap.sql
+++ db_script/bootstrap.sql
@@ -1,18 +1,31 @@
 --User
-insert into `User` (email, password, active, role) values( 'a@a.com', '123', true, 'ADMIN');
-insert into `User` (email, password, active, role) values( 's@a.com', '123', true, 'STAFF');
-insert into `User` (email, password, active, role) values( 'm@a.com', '123', true, 'MANAGER');
-insert into `User` (email, password, active, role) values( 'c@a.com', '123', true, 'CLIENT');
+insert into `User` (id, email, password, active, role) values(1, 'a@a.com', '123', true, 'ADMIN');
+insert into `User` (id, email, password, active, role) values(2, 's@a.com', '123', true, 'STAFF');
+insert into `User` (id, email, password, active, role) values(3, 'm@a.com', '123', true, 'MANAGER');
+insert into `User` (id, email, password, active, role) values(4, 'c@a.com', '123', true, 'CLIENT');
 
 --Urgency
-INSERT INTO Urgency(description) values('LOW')
-INSERT INTO Urgency(description) values('MEDIUM')
-INSERT INTO Urgency(description) values('HIGH')
+INSERT INTO Urgency(id, description) values(1, 'LOW');
+INSERT INTO Urgency(id, description) values(2, 'MEDIUM');
+INSERT INTO Urgency(id, description) values(3, 'HIGH');
 
 --Impact
-INSERT INTO Impact(description) values('LOW')
-INSERT INTO Impact(description) values('MEDIUM')
-INSERT INTO Impact(description) values('HIGH')
+INSERT INTO Impact(id, description) values(1, 'LOW');
+INSERT INTO Impact(id, description) values(2, 'MEDIUM');
+INSERT INTO Impact(id, description) values(3, 'HIGH');
 
 --Priority
---INSERT INTO Impact(description) values('MEDIUM')
+--priori 5
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(1, 'Low x Low', 172800, 86400, 1, 1);
+--priori 4
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(2, 'Low x Medium', 129600, 10800, 1, 2);
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(3, 'Medium x Low', 129600, 10800, 2, 1);
+--priori 3
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(4, 'Low x High', 86400, 7200, 1, 3);
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(5, 'High x Low', 86400, 7200, 3, 1);
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(6, 'Medium x Medium', 86400, 7200, 2, 2);
+--priori 2
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(7, 'Medium x High', 7200, 1200, 2, 3);
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(8, 'High x Medium', 7200, 1200, 3, 2);
+--priori 1
+INSERT INTO Priority(id, description, duration, response, urgency_id, impact_id) values(9, 'High x High', 3600, 600, 3, 3);
